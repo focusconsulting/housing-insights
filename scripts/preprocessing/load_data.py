6a4404d28dfd0c0d7838a19296522d2b59f5c960
NealHumphrey <humphrey.neal@gmail.com>
2016-11-03 16:18:29 -0400

    Adds ticket #14 of additional data uploading to Postgres database.
    A few misc other changes - downloaded Neighborhood_Clusters shapefiles, reworded some of the onboarding docs, started exploring joins with sql

--- scripts/preprocessing/load_data.py
+++ scripts/preprocessing/load_data.py
@@ -15,6 +15,7 @@ import json
 import pandas as pd
 from sqlalchemy import create_engine
 import csv
+import dateutil
 
 #Configure logging. See /logs/example-logging.py for examples of how to use this.
 logging_filename = "../logs/ingestion.log"
@@ -56,26 +57,39 @@ def csv_to_sql(index_path):
     engine = create_engine(connect_str)
 
     for index, row in paths_df.iterrows():
-        full_path = row['path'] + row['filename']
-        tablename = row['table_name']
+        if str(row['skip']) != "skip":
+            full_path = row['path'] + row['filename']
+            tablename = row['table_name']
 
-        logging.info("loading table " + str(index + 1) + " (" + tablename + ")")
+            logging.info("loading table " + str(index + 1) + " (" + tablename + ")")
 
-        #Since different files have different date columns, we need to compare to the master list.
-        headers = list(get_column_names(full_path))
-        with open(constants['date_headers_filename']) as fh:
-            date_headers = json.load(fh)
-            parseable_headers = date_headers['date_headers']
-        to_parse = list(set(parseable_headers) & set(headers))
-        logging.info("  identified date columns: " + str(to_parse))
+            #Since different files have different date columns, we need to compare to the master list.
+            headers = list(get_column_names(full_path))
+            with open(constants['date_headers_filename']) as fh:
+                date_headers = json.load(fh)
+                parseable_headers = date_headers['date_headers']
+            to_parse = list(set(parseable_headers) & set(headers))
+            logging.info("  identified date columns: " + str(to_parse))
 
-        #Custom date parser required to handle null dates.
-        parser = lambda x: pd.to_datetime(x, format='%m/%d/%Y', errors='coerce')
-        csv_df = pd.read_csv(full_path, encoding="latin_1", parse_dates=to_parse, date_parser=parser) #'Parcel_owner_date'
-        logging.info("  in memory...")
-        csv_df.to_sql(tablename, engine, if_exists='replace')
-        logging.info("  table loaded")
+            #Custom date parser required to handle null dates. Currently only allows one date format though.
+                #PresCat is m/d/yyyy
+                #Integreated_Tax is yyyy-mm-ddThh:mm:ss.000Z
+                #removed: format='%m/%d/%Y'
+            parser = lambda x: pd.to_datetime(x, errors='coerce', infer_datetime_format=True)
 
+            #Read the file into Pandas, and then reformat the column names to follow SQL conventions/best practice.
+            csv_df = pd.read_csv(full_path, encoding="latin_1", parse_dates=to_parse, date_parser=parser)
+            csv_df.columns = map(str.lower, csv_df.columns)
+            csv_df.columns = [c.replace(' ', '_') for c in csv_df.columns]
+            csv_df.columns = [c.replace('.', '_') for c in csv_df.columns]
+            logging.info("  in memory...")
+
+            #Send to the database
+            csv_df.to_sql(tablename, engine, if_exists='replace')
+            logging.info("  table loaded")
+
+        else:
+            logging.info("Row " + row['table_name'] + ": " + row['skip'])
 
 if __name__ == '__main__':
     #sample_add_to_database()
