f3fb90fe6cb900237959185aaf610c855a1ef67b
Brandon Walton <brandon.walton@blinemedical.com>
2017-01-20 14:02:45 -0500

    add test data csv and try importing csv using the csv library

--- scripts/ingestion/demo_load_to_sql_without_pandas.py
+++ scripts/ingestion/demo_load_to_sql_without_pandas.py
@@ -1,5 +1,6 @@
 
-
+import csv
+import argparse
 '''
 Use hard-coded data like file = filename.csv or meta = {hardcoded_meta_for_one_table} to test out how to append data to a SQL table
 without using pandas_dataframe.to_sql().
@@ -10,3 +11,50 @@ Options:
  - some other tool?
 
  '''
+CLEAN, DIRTY, DROP = range(3)
+
+def clean_row(row):
+    """
+    Return a pair (state, row) if:
+    `CLEAN` if `row` was succesfully cleaned
+    `DIRTY` if `row` could not be cleaned
+    `DROP`  if `row` was empty.
+    """
+    # Sanitize data
+    for idx, field in enumerate(row):
+        print idx, field
+        if field == 'NA_string':
+            row[idx] = ''
+    print row
+    return CLEAN, row
+
+
+def clean_csv(input_file, clean, dirty):
+    """
+    Read rows from the csv file. Clean rows are written to a
+    clean output file and dirty rows are written to a dirty
+    output file.
+    """
+    clean_writer = csv.writer(clean)
+    dirty_writer = csv.writer(dirty)
+    headings = "street_address, city, latitude, longitude".split(',')
+    for writer in clean_writer, dirty_writer:
+        writer.writerow(headings)
+
+    # Start processing rows
+    for row in csv.reader(input_file):
+        disposition, row = clean_row(row)
+        if disposition == CLEAN:
+            clean_writer.writerow(row)
+        elif disposition == DIRTY:
+            dirty_writer.writerow(row)
+        else:
+            assert disposition == DROP
+
+if __name__ == '__main__':
+    PARSER = argparse.ArgumentParser(description=clean_csv.__doc__)
+    PARSER.add_argument('INPUT', type=argparse.FileType('r'), help='Input CSV file')
+    PARSER.add_argument('CLEAN', type=argparse.FileType('w'), help='Clean output file')
+    PARSER.add_argument('DIRTY', type=argparse.FileType('w'), help='Dirty output file')
+    ARGUMENTS = PARSER.parse_args()
+    clean_csv(ARGUMENTS.INPUT, ARGUMENTS.CLEAN, ARGUMENTS.DIRTY)
