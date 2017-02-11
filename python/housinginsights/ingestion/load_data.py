fa735cc5751d765893cdb55e0b7b9f96ddb7c50f
blw9u2012 <brandonlwalton@gmail.com>
2017-02-10 22:08:54 -0500

    add pep8 formatting suggestions

--- python/housinginsights/ingestion/load_data.py
+++ python/housinginsights/ingestion/load_data.py
@@ -1,5 +1,5 @@
 ##########################################################################
-## Summary
+# Summary
 ##########################################################################
 
 '''
@@ -16,34 +16,37 @@ Notes:
 '''
 
 ##########################################################################
-## Imports & Configuration
+# Imports & Configuration
 ##########################################################################
-#external imports
+# external imports
 import logging
 import json
 import csv
 from urllib.request import urlretrieve
 
-#Needed to make relative package imports when running this file as a script (i.e. for testing purposes). Read why here: https://www.blog.pythonlibrary.org/2016/03/01/python-101-all-about-imports/
+# Needed to make relative package imports when running this file as a script (i.e. for testing purposes). Read why here:
+# https://www.blog.pythonlibrary.org/2016/03/01/python-101-all-about-imports/
 if __name__ == '__main__':
     import sys, os
     sys.path.append(os.path.abspath('../../'))
 
-#Relative package imports
+# Relative package imports
 from housinginsights.tools import database
 
-#configuration
-#See /logs/example-logging.py for usage examples
+# configuration
+# See /logs/example-logging.py for usage examples
 logging_filename = "../../logs/ingestion.log"
 logging.basicConfig(filename=logging_filename, level=logging.DEBUG)
-logging.getLogger().addHandler(logging.StreamHandler())     #Pushes everything from the logger to the command line output as well.
+
+# Pushes everything from the logger to the command line output as well.
+logging.getLogger().addHandler(logging.StreamHandler())
 
 
 #############################
-#FUNCTIONS
+# FUNCTIONS
 #############################
 
-#Completed, tests created. 
+# Completed, tests created.
 def do_fields_match(data_row, meta, table_name):
     '''
     data_row = a dictionary representing one row of data, with the dict keys as header names
@@ -55,32 +58,32 @@ def do_fields_match(data_row, meta, table_name):
     field_list = meta[table_name]['fields']
     included = {}
 
-    #Initialize values - start out assuming all is OK until we identify problems. 
-    return_value = True                     
+    # Initialize values - start out assuming all is OK until we identify problems.
+    return_value = True
     not_found = []
 
-    #Check that all of the data columns are in the meta.json
+    # Check that all of the data columns are in the meta.json
     for field in data_row:
         if not any(d.get('source_name', None) == field for d in field_list):
             not_found.append('"{}" in CSV not found in meta'.format(field))
             return_value = False
 
-    #Check that all the meta.json columns are in the data
+    # Check that all the meta.json columns are in the data
     for field in field_list:
         if field['source_name'] not in data_row:
             not_found.append('"{}" in meta.json not found in data'.format(field['source_name']))
             return_value = False
-    
-    #Log our errors if any
+
+    # Log our errors if any
     if return_value == False:
         logging.warning("do_fields_match: {}. '{}' had missing items:\n{}".format(return_value, table_name, not_found))
     else:
         logging.info("do_fields_match: {}. meta.json and csv field lists match completely for '{}'".format(return_value, table_name))
-    
+
     return return_value
 
 
-#Completed, tests not written.
+# Completed, tests not written.
 def load_meta_data(filename='meta.json'):
     '''
     Expected meta data format:
@@ -113,7 +116,8 @@ def load_meta_data(filename='meta.json'):
 
     return meta
 
-#Completed, tests not written
+
+# Completed, tests not written
 def read_csv_manifest(filename = 'manifest.csv'):
     '''
     Reads each line of the csv manifest, which specifies which files should be loaded
@@ -125,7 +129,8 @@ def read_csv_manifest(filename = 'manifest.csv'):
         for row in reader:
             yield row
 
-#Completed, test created
+
+# Completed, test created
 def check_csv_manifest_unique_ids(filename = 'manifest.csv'):
     '''
     Makes sure that every value in the manifest column 'unique_data_id' is in fact unique. 
@@ -142,7 +147,8 @@ def check_csv_manifest_unique_ids(filename = 'manifest.csv'):
 
     return True
 
-#Completed, tests incomplete
+
+# Completed, tests incomplete
 def get_sql_manifest_row(database_connection, csv_row):
     '''
     Loads the row from the manifest table that matches the unique_data_id in csv_row.
@@ -151,27 +157,28 @@ def get_sql_manifest_row(database_connection, csv_row):
 
     unique_data_id = csv_row['unique_data_id']
     sql_query = "SELECT * FROM manifest WHERE unique_data_id = '{}'".format(unique_data_id)
-    
-    #temp
-    #sql_query = "SELECT * FROM manifest WHERE unique_data_id = '{}'".format('my_data_id')
+
+    # temp
+    # sql_query = "SELECT * FROM manifest WHERE unique_data_id = '{}'".format('my_data_id')
     logging.info(sql_query)
     query_result = database_connection.execute(sql_query)
 
-    #convert the sqlAlchemy ResultProxy object into a list of dictionaries
+    # convert the sqlAlchemy ResultProxy object into a list of dictionaries
     results = [dict(row.items()) for row in query_result]
-    
-    #We expect there to be exactly one row matching the query if the csv_row is already in the database
+
+    # We expect there to be exactly one row matching the query if the csv_row is already in the database
     if len(results)>1:
         raise ValueError('Found multiple rows in database for data id {}'.format(unique_data_id))
-    
-    #Return just the dictionary of results, not the list of dictionaries
+
+    # Return just the dictionary of results, not the list of dictionaries
     if len(results)==1:
-        return results[0] 
+        return results[0]
 
     if len(results)==0:
         return None
 
-#incomplete
+
+# incomplete
 def update_sql_manifest(database_connection, status = None):
     '''
     If the 'manifest' table does not exist in SQL, create it.
@@ -185,7 +192,8 @@ def update_sql_manifest(database_connection, status = None):
     '''
     pass
 
-#complete, tests not written
+
+# complete, tests not written
 def should_file_be_loaded(csv_row, sql_row):
     '''
     If the sql_manifest_table indicates that this file is already loaded into this copy of the database, returns False
@@ -198,14 +206,14 @@ def should_file_be_loaded(csv_row, sql_row):
         if sql_row['include_flag'] != 'loaded':
             return True
         if sql_row['include_flag'] == 'loaded':
-            logging.info("{} is already in the database, skipping".format(csv_row['unique_data_id'])) 
+            logging.info("{} is already in the database, skipping".format(csv_row['unique_data_id']))
             return False
     else:
-        logging.info("{} include_flag is {}, skipping".format(csv_row['unique_data_id'], csv_row['include_flag'])) 
+        logging.info("{} include_flag is {}, skipping".format(csv_row['unique_data_id'], csv_row['include_flag']))
         return False
 
 
-#complete, tests not written
+# complete, tests not written
 def download_data_file(csv_row):
     '''
     Checks to see if the file already exists locally.
@@ -215,7 +223,7 @@ def download_data_file(csv_row):
     '''
     from urllib.request import urlretrieve
 
-    #TODO make sure that both of these can handle trailing/leading slashes appropriately
+    # TODO make sure that both of these can handle trailing/leading slashes appropriately
     local_path = os.path.join(os.path.dirname(__file__), csv_row['local_folder'], csv_row['filepath'])
     s3_path = csv_row['s3_folder'] + csv_row['filepath']
 
@@ -232,9 +240,11 @@ def download_data_file(csv_row):
             myreader=csv.reader(f,delimiter=',')
             headers = next(myreader)
 
-    return headers #not strictly necessary, but if you want to verify the file
+    # not strictly necessary, but if you want to verify the file
+    return headers
 
-#incomplete
+
+# incomplete
 def csv_to_sql(manifest_row, engine, meta):
     """
     Does the actual loading of the file into SQL. This assumes all necessary tests have been passed (i.e. do_fields_match etc.)
@@ -246,7 +256,8 @@ def csv_to_sql(manifest_row, engine, meta):
     """
     pass
 
-#complete, tests not written
+
+# complete, tests not written
 def drop_tables(database_choice):
     '''
     used to rebuild the database by first dropping all tables before running main()
@@ -255,6 +266,7 @@ def drop_tables(database_choice):
     engine = database_management.create_engine(connect_str)
     database_connection = engine.connect()
     query_result = database_connection.execute("DROP SCHEMA public CASCADE;CREATE SCHEMA public;")
+
 
 def main(database_choice):
 
@@ -275,7 +287,6 @@ def main(database_choice):
     - at the end of loading print an error message to the console
     '''
 
-    
     meta = load_meta_data('meta_sample.json')
     database_connection = tools.database.get_database_connection('local_database')
 
@@ -284,62 +295,59 @@ def main(database_choice):
     for idx, csv_row in enumerate(read_csv_manifest()):
         sql_row = get_sql_manifest_row(database_connection = database_connection, csv_row = csv_row)
 
-        #only load the file if it is not already in the database
+        # only load the file if it is not already in the database
         if not should_file_be_loaded(sql_row=sql_row, csv_row = csv_row):
-            pass #specific reason will be logged by should_file_be_loaded
+            pass # specific reason will be logged by should_file_be_loaded
         else:
             table_name = csv_row['table_name']
             download_data_file(csv_row)             #performs the download if necessary, throws error if it can't open the file after downloading
-            
+
             reader = DataReader()
-            able_to_load = True  #TODO it would be good to attach this as a property to the DataReader. If any problem occurs we can abandon 
+            able_to_load = True  #TODO it would be good to attach this as a property to the DataReader. If any problem occurs we can abandon
             for index, data_row in enumerate(DataReader()):
-                #When we first access the data, check the headers match.
+                # When we first access the data, check the headers match.
                 if index == 0:
                     if not do_fields_match(data_row=data_row, meta=meta, table_name=table_name):
                         logging.info('data fields in {} do not match meta.json')
                         able_to_load = False
-                        break       #abandom this file
+                        # abandon this file
+                        break
 
-                #Continue the for loop - clean rows and add them to cleaned.csv
+                # Continue the for loop - clean rows and add them to cleaned.csv
                 pass
-                #TODO normalize the date functions for all date field types
-                #TODO apply cleaning functions specific to this data file
-                #TODO write the row to a temporary cleaned.csv file
+                # TODO normalize the date functions for all date field types
+                # TODO apply cleaning functions specific to this data file
+                # TODO write the row to a temporary cleaned.csv file
 
 
             if ready_to_load == True:
-                #TODO write the cleaned.csv to the appropriate SQL table
-                #TODO add/update the appropriate row to the SQL manifest table indicating new status
+                # TODO write the cleaned.csv to the appropriate SQL table
+                # TODO add/update the appropriate row to the SQL manifest table indicating new status
                 pass
 
-
-
-        #temporary code 
+        # temporary code
         if idx > 10: break
-        print (csv_row)
-
-
+        print(csv_row)
 
 
 if __name__ == '__main__':
 
-    #Eventual structure:
-    database_choice = 'local_database' #the appropriate database name in secrets.json. Should make this changeable via optional positional sys.argv
+    # Eventual structure:
+    # the appropriate database name in secrets.json. Should make this changeable via optional positional sys.argv
+    database_choice = 'local_database'
 
     if 'rebuild' in sys.argv:
         drop_tables(database_choice)
 
     main(database_choice)
 
-
-    #------------------
-    #testing code follows
-    #------------------
+    # ------------------
+    # testing code follows
+    # ------------------
 
     database_connection = database.get_database_connection('local_database')
     MANIFEST_FILENAME = os.path.join(os.path.dirname(__file__), 'manifest_sample.csv')
-    
+
     if 'download_test' in sys.argv:
         manifest = read_csv_manifest(MANIFEST_FILENAME)
         csv_row_1 = next(manifest)
