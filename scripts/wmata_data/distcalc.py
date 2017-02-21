411ebd4da7efd353f904eaf942df7c9ef6186c49
Priscilla Alexander <palexander@learningobjects.com>
2017-02-21 11:38:47 -0500

    refactored methods to be more consistent and reading from secrets.json for API keys

--- scripts/wmata_data/distcalc.py
+++ scripts/wmata_data/distcalc.py
@@ -1,10 +1,7 @@
 import sys
 import csv
+import json
 import requests
-
-#TODO replace these with code4dc keys
-WMATA_HEADERS = {'api_key': ''}
-MAPBOX_PARAMS = {'access_token': ''}
 
 ###### Converts meters to miles
 def getMiles(i):
@@ -14,49 +11,63 @@ def getMiles(i):
 def getMeters(i):
     return i*1609.344
 
+def getWmataHeaders(wmata_api_key):
+    return { 'api_key': wmata_api_key }
+
 ###### Gets walking distance between two locations in meters
-def getWalkingDistance(srcLat, srcLon, destLat, destLon):
+def getWalkingDistance(srcLat, srcLon, destLat, destLon, mapbox_api_key):
     distReqCoords = srcLon + ',' + srcLat + ';' + destLon + ',' + destLat;
+
+    mapbox_params = {'access_token':mapbox_api_key}
+
 
     #according to documentation, this doesn't work in Python SDK so switched to using REST API
     walkDistResponse = requests.get("https://api.mapbox.com/directions/v5/mapbox/walking/"+distReqCoords,
-                                params=MAPBOX_PARAMS)
+                                params=mapbox_params)
     return walkDistResponse.json()['routes'][0]['legs'][0]['distance']
 
 
 ###### Finds all rail stations within {radiusinmeters} from the given project. Writes to given CSV file.
-def findRailStations(railStations,project,radiusinmeters,distCsvWriter):
+def findRailStations(railStations,project,radiusinmeters,distCsvWriter, mapbox_api_key):
     lat = project['Proj_lat']
     lon = project['Proj_lon']
 
     for station in railStations:
-        walkDist = getWalkingDistance(lat, lon, str(station['Lat']), str(station['Lon']))
+        walkDist = getWalkingDistance(lat, lon, str(station['Lat']), str(station['Lon']), mapbox_api_key)
         if walkDist <= radiusinmeters:
             distCsvWriter.writerow((project['Nlihc_id'],'rail',station['Code'],"{0:.2f}".format(getMiles(walkDist))))
 
 
 ###### Finds all bus stations within {radiusinmeters} from the given project. Writes to given CSV file.
-def findBusStations(project, radiusinmeters, distCsvWriter):
+def findBusStations(project, radiusinmeters, distCsvWriter, wmata_api_key, mapbox_api_key):
     lat = project['Proj_lat']
     lon = project['Proj_lon']
+
+    wmata_headers = getWmataHeaders(wmata_api_key)
 
     params = {'Lat': lat,
               'Lon' : lon,
               'Radius': str(radiusinmeters)}
-    response = requests.get('https://api.wmata.com/Bus.svc/json/jStops',params=params, headers=WMATA_HEADERS)
+    response = requests.get('https://api.wmata.com/Bus.svc/json/jStops',params=params, headers=wmata_headers)
     data = response.json()
 
     for stop in data['Stops']:
-        walkDist = getWalkingDistance(lat, lon, str(stop['Lat']), str(stop['Lon']))
+        walkDist = getWalkingDistance(lat, lon, str(stop['Lat']), str(stop['Lon']), mapbox_api_key)
 
         if walkDist <= radiusinmeters: #within 0.5 miles walking
             distCsvWriter.writerow((project['Nlihc_id'],'bus',stop['StopID'],"{0:.2f}".format(getMiles(walkDist))))
 
 
 ####### Writes rail station metadata to given CSV writer
-def writeRailInfo(railStations, infoCsvWriter):
+### Returnst the railStations info...
+def writeRailInfo(infoCsvWriter, wmata_api_key):
 
     print("Writing RAIL INFO");
+
+    wmata_headers = getWmataHeaders(wmata_api_key)
+
+    railResponse = requests.get("https://api.wmata.com/Rail.svc/json/jStations",headers =wmata_headers)
+    railStations = railResponse.json()['Stations']
 
     for station in railStations:
         #delimit list of lines with colon
@@ -72,13 +83,18 @@ def writeRailInfo(railStations, infoCsvWriter):
         if station['LineCode4'] != None:
             lines = '{}:{}'.format(lines, station['LineCode4'])
 
-        infoCsvWriter.writerow((station['Code'],station['Name'],str(station['Lat']),
+        infoCsvWriter.writerow((station['Code'],'rail',station['Name'],str(station['Lat']),
                                                         str(station['Lon']),lines))
+
+    return railStations
+
 ####### Writes bus station metadata to given CSV writer
-def writeBusInfo(infoCsvWriter):
+def writeBusInfo(infoCsvWriter, wmata_api_key):
     print("Writing BUS INFO")
 
-    response = requests.get('https://api.wmata.com/Bus.svc/json/jStops', headers=WMATA_HEADERS)
+    wmata_headers = getWmataHeaders(wmata_api_key)
+
+    response = requests.get('https://api.wmata.com/Bus.svc/json/jStops', headers=wmata_headers)
     data = response.json()
 
     for stop in data['Stops']:
@@ -88,32 +104,34 @@ def writeBusInfo(infoCsvWriter):
             lines = '{}:{}'.format(lines,route)
         lines = lines[1:] #take off the first :
 
-        infoCsvWriter.writerow((stop['StopID'],stop['Name'],stop['Lat'],stop['Lon'],lines))
-
+        infoCsvWriter.writerow((stop['StopID'],'bus',stop['Name'],stop['Lat'],stop['Lon'],lines))
 
 
 ########### MAIN #############
-def main(csvInputFileName,distOutputFileName,infoOutputFileName):
+def main(secretsFileName, csvInputFileName,distOutputFileName,infoOutputFileName):
+
+    #pull API keys
+    api_keys = json.loads(open(secretsFileName).read())
+    wmata_api_key = api_keys['wmata']['api_key']
+    mapbox_api_key = api_keys['mapbox']['public-token']
+
+    #write out the wmata info csv
+    infoOutputFile = open(infoOutputFileName, 'wt')
+    infoCsvWriter = csv.writer(infoOutputFile)
+    infoCsvWriter.writerow(('code_or_id','type','name','lat','lon','lines'))
+    #saving railStations to compute distances from each project later in the script. reduces network calls.
+    railStations = writeRailInfo(infoCsvWriter, wmata_api_key)
+    writeBusInfo(infoCsvWriter, wmata_api_key)
 
     projectsFile = open(csvInputFileName)
     distOutputFile = open(distOutputFileName, 'wt')
     distCsvWriter = csv.writer(distOutputFile)
 
-    infoOutputFile = open(infoOutputFileName, 'wt')
-    infoCsvWriter = csv.writer(infoOutputFile)
-
     reader = csv.DictReader(projectsFile)
 
-    numrow = 0
-
-    #print headers
     distCsvWriter.writerow(('Nlihc_id','type','stop_id_or_station_code','dist_in_miles'))
-    infoCsvWriter.writerow(('code_or_id','type','name','lat','lon','lines'))
 
-    railResponse = requests.get("https://api.wmata.com/Rail.svc/json/jStations",headers =WMATA_HEADERS)
-    railStations = railResponse.json()['Stations']
-    writeRailInfo(railStations, infoCsvWriter)
-    writeBusInfo(infoCsvWriter)
+    numrow = 0
 
     for row in reader:
         radius = getMeters(0.5)
@@ -126,24 +144,26 @@ def main(csvInputFileName,distOutputFileName,infoOutputFileName):
 
         # find all metro stations within 0.5 miles
         print("Starting processing rail stations for {}".format(numrow))
-        findRailStations(railStations,row,radius,distCsvWriter)
+        findRailStations(railStations,row,radius,distCsvWriter, mapbox_api_key)
         print("Completed processing rail stations for {}".format(numrow))
 
 
         # find all bus stops within 0.5 miles
         print("Starting processing bus stations for {}".format(numrow))
-        findBusStations(row, radius, distCsvWriter)
+        findBusStations(row, radius, distCsvWriter, wmata_api_key, mapbox_api_key)
         print("Completed processing bus stations for {}".format(numrow))
 
 
-if len(sys.argv) < 4:
-    print("Requires 3 arguments: [csv input file] [WMATA_DIST output file] [WMATA_INFO output file]")
+if len(sys.argv) < 5:
+    print("Requires 4 arguments: [csv input file] [WMATA_DIST output file] [WMATA_INFO output file] [secrets.json]")
 else:
     inputFileName = sys.argv[1]
     distOutputFileName = sys.argv[2]
     infoOutputFileName = sys.argv[3]
+    secretsFileName = sys.argv[4]
+
     print("Will read from {}".format(inputFileName))
     print("Will write WMATA_DIST table to {}".format(distOutputFileName))
     print("Will write WMATA_INFO table to {}".format(infoOutputFileName))
 
-    main(inputFileName, distOutputFileName, infoOutputFileName)
+    main(secretsFileName, inputFileName, distOutputFileName, infoOutputFileName)
