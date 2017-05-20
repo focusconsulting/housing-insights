69183590fc461f6b044e913283d4c080b9ee03be
ilissablechwapo <ilissablech@gmail.com>
2017-05-20 13:51:40 -0400

    test cama api

---
+++ python/housinginsights/sources/cama.py
@@ -0,0 +1,221 @@
+from pprint import pprint
+import os
+import sys
+import requests
+
+
+PYTHON_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir))
+sys.path.append(PYTHON_PATH)
+
+from housinginsights.sources.base import BaseApiConn
+from housinginsights.sources.models.mar import MarResult, FIELDS
+
+
+class CamaApiConn(BaseApiConn):
+    """
+    API Interface to the Master Address Record (MAR) database.
+    Use public methods to retrieve data.
+    """
+
+    BASEURL = 'http://citizenatlas.dc.gov/newwebservices/locationverifier.asmx'
+
+    def __init__(self):
+        super().__init__(CamaApiConn.BASEURL)
+
+    def get_data(self, square, lot, output_type=None,
+                      output_file=None):
+        """
+        Get information on a location based on a simple query string.
+
+        :param square: SSL first part
+        :type  location: String.
+
+        :param lot: SSL second part
+        :type  location: String.
+
+        :param output_type: Output type specified by user.
+        :type  output_type: String.
+
+        :param output_file: Output file specified by user.
+        :type  output_file: String
+
+        :returns: Json output from the api.
+        :rtype: String
+        """
+        params = {
+            'f': 'json',
+            'Square': square,
+            'Lot': lot,
+            'Suffix': 'null'
+        }
+        result = self.get('/findAddFromSSL2', params=params)
+        if result.status_code != 200:
+            err = "An error occurred during request: status {0}"
+            raise Exception(err.format(result.status_code))
+        if output_type == 'stdout':
+            pprint(result.json())
+        elif output_type == 'csv':
+            data = result.json()['returnDataset']['Table1']
+            results = [MarResult(address) for address in data]
+            self.result_to_csv(FIELDS, results, output_file)
+        return result.json()
+
+    def reverse_geocode(self, xcoord, ycoord, output_type=None,
+                        output_file=None):
+        """
+        Do a reverse geocode lookup for address/alias points within 200 meters
+        from the given Maryland State Plane (NAD 83) coordinates and returns the nearest five.
+        Returned distance is given in meters.
+
+        :param xcoord: Xcoordinate
+        :type xcoord: String
+
+        :param ycoord: Ycoordinate
+        :type  ycoord: String
+
+        :param output_type: Output type specified by user.
+        :type  output_type: String.
+
+        :param output_file: Output file specified by user.
+        :type  output_file: String
+
+        :returns: Json output from the api.
+        :rtype: String
+        """
+        params = {
+            'f': 'json',
+            'x': xcoord,
+            'y': ycoord
+        }
+        result = self.get('/reverseGeocoding2', params=params)
+        if result.status_code != 200:
+            err = "An error occurred during request: status {0}"
+            raise Exception(err.format(result.status_code))
+        if output_type == 'stdout':
+            pprint(result.json())
+        elif output_type == 'csv':
+            data = result.json()['Table1']
+            results = [MarResult(address) for address in data]
+            self.result_to_csv(FIELDS, results, output_file)
+        return result.json()
+
+    def get_condo_count(self, location, output_type=None,
+                        output_file=None):
+        """
+        Get a count of all the condos at a particular address.
+        Use sparingly, because it needs to make 3 requests
+        to get the information (this api tho...).
+
+        Get information on a location based on a simple query string.
+
+        :param location: Location query.
+        :type  location: String.
+
+        :param output_type: Output type specified by user.
+        :type  output_type: String.
+
+        :param output_file: Output file specified by user.
+        :type  output_file: String
+
+        :returns: Json output from the api.
+        :rtype: String
+        """
+        aid = self._get_address_id(location)
+        units = self._find_condo_unit(aid)
+        result = len(units['returnDataset']['Table1'])
+        if output_type == 'stdout':
+            print("Number of condos is: {0}".format(result))
+        return result
+
+    def get_condo_info(self, location, output_type=None,
+                       output_file=None):
+        pass  # TODO
+
+    def _find_condo_unit(self, address_id):
+        """
+        Find condo information from an address id.
+
+        :param address_id: Address id of the residence.
+        :type  address_id: String.
+        """
+        params = {
+            'f': 'json',
+            'AID': address_id
+        }
+        result = self.get('/FindCondoUnitFromAID2', params=params)
+        if result.status_code != 200:
+            err = "An error occurred during request: status {0}"
+            raise Exception(err.format(result.status_code))
+        return result.json()
+
+    def _get_address_id(self, location):
+        """
+        Get the address id of a location.
+
+        :param location: Location query string.
+        :type  location: String.
+
+        :returns: Address id
+        :rtype: String
+        """
+        result = self.find_location(location)
+        # Return the first result.
+        address_id = result['returnDataset']['Table1'][0]['ADDRESS_ID']
+        return address_id
+
+    def reverse_lat_lng_geocode(self, latitude, longitude, output_type=None,
+                                output_file=None):
+        """
+        Do a reverse geocode lookup for MAR address/alias points within 200 
+        meters from the given Latitude and Longitude coordinates and returns 
+        the nearest five. The returned distance unit is meter.
+
+        :param latitude: Latitude
+        :type latitude: str
+
+        :param longitude: Longitude
+        :type longitude: str
+        :param output_type: Output type specified by user.
+        :type  output_type: str
+
+        :param output_file: Output file specified by user.
+        :type  output_file: str
+
+        :returns: Json output from the api.
+        :rtype: json
+        """
+        params = {
+            'f': 'json',
+            'lat': latitude,
+            'lng': longitude
+        }
+        result = self.get('/reverseLatLngGeocoding2', params=params)
+        if result.status_code != 200:
+            err = "An error occurred during request: status {0}"
+            raise Exception(err.format(result.status_code))
+        if output_type == 'stdout':
+            pprint(result.json())
+        elif output_type == 'csv':
+            data = result.json()['Table1']
+            results = [MarResult(address) for address in data]
+            self.result_to_csv(FIELDS, results, output_file)
+        return result.json()
+
+def cama_data():
+
+    my_api = CamaApiConn()
+
+    residential = 'https://opendata.arcgis.com/datasets/c5fb3fbe4c694a59a6eef7bf5f8bc49a_25.geojson'
+    r = requests.get(residential)
+    residential_data = r.json()
+
+    dict_res = {}  # creates dict of residential data with SSL as primary key
+    for row in residential_data['features']:
+        dict_res.setdefault(row['properties']['SSL'], {})
+        square, lot = row['properties']['SSL'].split('    ')
+        dict_res = my_api.get_data(square, lot)
+        #dict_res[row['properties']['SSL']] = row['properties']
+
+cama_data()
+
+#data = my_api.get_data('findAddFromSSL2')
