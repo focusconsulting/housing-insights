2859e71fb8141e9e81a98fefc6dfd98bb2eac3a4
NealHumphrey <humphrey.neal@gmail.com>
2017-05-22 15:21:17 -0400

    trying to get it to not merge in changes

--- python/housinginsights/sources/base_notmelana.py
+++ python/housinginsights/sources/base_notmelana.py
@@ -1,3 +1,22 @@
+'''
+this is the copy of the file that was there before the latest merge. 
+
+Let's make it look really different. 
+
+Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium 
+doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi
+ architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernat
+ ur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi
+  nesciunt, neque porro quisquam est, qui dolorem ipsum, quia dolor sit amet, consectetur
+  , adipisci[ng] velit, sed quia non numquam [do] eius modi tempora inci[di]dunt, ut lab
+  ore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum 
+  exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatu
+  r? Quis autem vel eum iure reprehenderit, qui in ea voluptate velit esse, quam nih
+  il molestiae
+   consequatur, vel illum, qui dolorem eum fugiat, quo voluptas nulla pariatur?
+'''
+
+
 """
 Base Api Connection classes.
 """
@@ -82,6 +101,22 @@ class BaseApiConn(object):
         if not os.path.isdir(directory):
             os.mkdir(directory)
 
+
+
+'''
+  , adipisci[ng] velit, sed quia non numquam [do] eius modi tempora inci[di]dunt, ut lab
+  ore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum 
+  exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatu
+  r? Quis autem vel eum iure reprehenderit, qui in ea voluptate velit esse, quam nih
+  il molestiae
+   consequatur, vel illum, qui dolorem eum fugiat, quo voluptas nulla pariatur?
+'''
+
+
+
+
+
+
     def result_to_csv(self, fields, results, filepath):
         """
         Write the data to a csv file.
@@ -119,6 +154,15 @@ class BaseApiConn(object):
         self.create_directory_if_missing(filepath)
         with open(filepath, 'w', encoding='utf-8') as f:
             f.write(data)
+
+'''
+  , adipisci[ng] velit, sed quia non numquam [do] eius modi tempora inci[di]dunt, ut lab
+  ore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum 
+  exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatu
+  r? Quis autem vel eum iure reprehenderit, qui in ea voluptate velit esse, quam nih
+  il molestiae
+   consequatur, vel illum, qui dolorem eum fugiat, quo voluptas nulla pariatur?
+'''
 
 
 class BaseApiManager(object):
