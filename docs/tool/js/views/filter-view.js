52f0cf7c81c3a0bc83ea286c26b4724161387579
NealHumphrey <humphrey.neal@gmail.com>
2017-05-24 11:30:31 -0400

    Changes:
    - Fixed the ES6 issue w/ publishing state changes to filtering by using a closure
    - Hooked up the /util/filter.js file to the filterValue state change
    - Commented out the current incomplete filter method
    - Added a demonstration list of filter values to show that deduplication util method works correctly
    - Added 'data_type' to the components config data so that the util/filter.js will be able to distinguish between decimal and integer fields (since both get a continuous slider but filter logic needs to be implemented slightly differently)

--- docs/tool/js/views/filter-view.js
+++ docs/tool/js/views/filter-view.js
@@ -5,33 +5,47 @@ var filterView = {
     components: [
         //TODO this hard coded array of objects is just a temporary method. 
         //This should be served from the API, probably from meta.json
+        //"source" is the column name from the filter table, which should match the table in the original database table. 
+        //  For this approach to work, it will be cleanest if we never have duplicate column names in our sql tables unless the data has
+        //  the same meaning in both places (e.g. 'ward' and 'ward' can appear in two tables but should have same name/format)
+
         {   source: 'proj_units_tot',
             display_name: 'Project unit count',
             component_type: 'continuous',
+            data_type:'integer',
             min: 5,
             max: 200,
             num_decimals_displayed: 0 //0 if integer, 1 otherwise. Could also store data type instead. 
         },
-        {   source: 'proj_units_assist_max',
-            display_name: 'Subsidized units',
+        {   source: 'acs_rent_median',
+            display_name: 'Neighborhood Rent (ACS median)',
             component_type: 'continuous',
+            data_type:'decimal',
             min: 0,
-            max: 150,
+            max: 2500,
             num_decimals_displayed: 0 //0 if integer, 1 otherwise. Could also store data type instead. 
         },
         {   source:'poa_start',
             component_type: 'date',
+            data_type: 'timestamp',
             min: '1950-01-01', //just example, TODO change to date format
             max: 'now'         //dummy example
         },
         {   source:'ward',
             component_type: 'categorical',
+            data_type: 'text'
             other: 'feel free to add other needed properties if they make sense'
         }
     ],
 
     init: function() {
         
+        //Make sure other functionality is hooked up
+        setSubs([
+            ['filterViewLoaded', filterUtil.init]
+        ]);
+
+
         //Add components to the navigation using the appropriate component type
         //TODO later we'll need to make sure this uses the appropriate order
         for (var i = 0; i < filterView.components.length; i++) {
@@ -40,9 +54,7 @@ var filterView = {
             //Set up sliders
             if (filterView.components[i].component_type == 'continuous'){
                 
-                //TODO ES6 only, see comment in sliderMove for discussion
-                //local scope alias. c is for component
-                let c = filterView.components[i]
+                var c = filterView.components[i]
                 
                 console.log("Found a continuous source!");
 
@@ -73,29 +85,27 @@ var filterView = {
 
                 });
 
-                //Publish changes to the StateModule
-                function sliderMove ( values, handle, unencoded, tap, positions ) {
-                    // This is the custom binding module used by the noUiSlider.on() callback. 
+                //Each slider needs its own copy of the sliderMove function so that it can use the current component
+                function makeSliderCallback(component){
+                    return function sliderCallback ( values, handle, unencoded, tap, positions ) {
+                        // This is the custom binding module used by the noUiSlider.on() callback. 
 
-                    // values: Current slider values;
-                    // handle: Handle that caused the event;
-                    // unencoded: Slider values without formatting;
-                    // tap: Event was caused by the user tapping the slider (boolean);
-                    // positions: Left offset of the handles in relation to the slider
-
-                    //TODO the challenge here is how do we get the property of which slider was actually called inside
-                    //this sliderMove function itself?? We can't change method signature because it is defined
-                    //by noUiSlider. Maybe we need a constructor to produce a copy of this function for each slider?
-                    //If we go with ES6, using 'let' on the for loop will make the slider variable locally scoped 
-                    //which solves the problem. Currently using this approach
-                    //TODO are we doing only ES6 compatibility???
-                    var specific_state_code = 'filterValues.' + c.source //ES6 only.... 
-                    setState(specific_state_code,unencoded); 
+                        // values: Current slider values;
+                        // handle: Handle that caused the event;
+                        // unencoded: Slider values without formatting;
+                        // tap: Event was caused by the user tapping the slider (boolean);
+                        // positions: Left offset of the handles in relation to the slider
+                        var specific_state_code = 'filterValues.' + component.source
+                        setState(specific_state_code,unencoded); 
+                    }
                 }
+
+                //Construct a new copy of the function with access to the current c variable
+                var currentSliderCallback = makeSliderCallback(c)
 
                 //Using 'set' only updates on release. Probably better to use the 'update' method for continuous updates. 
                 //using 'set' for now for easier development (less console logging of state changes)
-                slider.noUiSlider.on('set', sliderMove);
+                slider.noUiSlider.on('set', currentSliderCallback);
 
             };//end slider setup
 
@@ -118,8 +128,9 @@ var filterView = {
         }; //end for loop. All components on page.
 
 
+        //After all filter components are loaded, user is allowed to filter data
+        setState('filterViewLoaded',true);
+
     } //end init
 };
 
-console.log("loaded filter-view")
-console.log(filterView)
