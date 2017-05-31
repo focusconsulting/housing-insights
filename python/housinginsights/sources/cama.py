8da6823c2db43b410ce552398a89b3c28906c18f
Louis Kwon <hanlerris@gmail.com>
2017-05-31 00:08:08 -0400

    cama.py changed to send csv file to data/processed/zoneUnitCount folder

--- python/housinginsights/sources/cama.py
+++ python/housinginsights/sources/cama.py
@@ -4,6 +4,7 @@ import sys
 import requests
 from collections import OrderedDict
 import csv
+import datetime
 
 PYTHON_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir))
 sys.path.append(PYTHON_PATH)
@@ -54,11 +55,11 @@ class MarApiConn(BaseApiConn):
             mar_returns = {'Warning': 'No MAR data availble - property under construction - see AYB year'}
         else:
             entry = mar_data['returnDataset']['Table1'][0]
-            mar_returns = {'ANC': entry['ANC'],
-                           'CENSUS_TRACT': entry['CENSUS_TRACT'],
-                           'CLUSTER_': entry['CLUSTER_'],
-                           'WARD': entry['WARD'],
-                           'ZIPCODE': entry['ZIPCODE']
+            mar_returns = {'anc': entry['ANC'],
+                           'census_tract': entry['CENSUS_TRACT'],
+                           'neighborhood_cluster': entry['CLUSTER_'],
+                           'ward': entry['WARD'],
+                           'zip': entry['ZIPCODE']
             }
 
         return mar_returns
@@ -74,6 +75,13 @@ class CamaApiConn(BaseApiConn):
         super().__init__(CamaApiConn.BASEURL)
 
     def get_data(self):
+        """
+        Grabs data from CAMA. Individual CAMA property retrieves zone_type data
+        from MAR api. Count number of housing units and bedroom units per zone.
+        Return the count data (in dictionary form) to be processed into csv
+        by get_csv() method.
+        """
+
         mar_api = MarApiConn()
         result = self.get(urlpath='/c5fb3fbe4c694a59a6eef7bf5f8bc49a_25.geojson', params=None)
 
@@ -82,16 +90,26 @@ class CamaApiConn(BaseApiConn):
             raise Exception(err.format(result.status_code))
         cama_data = result.json()
 
-        dict_res = {}  # creates dict of residential data with SSL as primary key
-        zone_types = ['ANC', 'CENSUS_TRACT', 'CLUSTER_', 'WARD', 'ZIPCODE']
+        """
+        Example of: anc_count = [OrderedDict([('zone_type', 'anc'), ('zone', 'ANC 2B'), ('housing_unit_count', 10), ('bedroom_unit_count', 10)], etc)]
+        """
+        zone_types = ['anc', 'census_tract', 'neighborhood_cluster', 'ward', 'zip']
         anc_count = []
         census_count = []
         cluster_count = []
         ward_count = []
         zipcode_count = []
 
+        """
+        Take each CAMA property data and retrieve the MAR data.
+        """
+        """
+        Certain square values have four digits + a letter. (ex. 8888E)
+        Square would be the first four digits and suffix would be the letter.
+        """
+
         """ [:100] for the first 100 cama_data points """
-        for row in cama_data['features'][:100]:
+        for row in cama_data['features']:
             objectid = row['properties']['OBJECTID']
             square, lot = row['properties']['SSL'].split()
             suffix = ' '
@@ -100,22 +118,19 @@ class CamaApiConn(BaseApiConn):
                 suffix = square[-1]
 
             mar_return = mar_api.get_data(square, lot, suffix)
-            row['properties'].update(mar_return)
-            dict_res[row['properties']['OBJECTID']] = row['properties']
 
-
-            ''' Count the units and bedrooms '''
+            ''' Count the housing units and bedrooms '''
             num_units = row['properties']['NUM_UNITS']
             if num_units == 0: num_units = 1
             bedrm = row['properties']['BEDRM']
             if bedrm == 0: bedrm = 1
 
             for zone in zone_types:
-                if zone == 'ANC': zone_count = anc_count
-                elif zone == 'CENSUS_TRACT': zone_count = census_count
-                elif zone == 'CLUSTER_': zone_count = cluster_count
-                elif zone == 'WARD': zone_count = ward_count
-                elif zone == 'ZIPCODE': zone_count = zipcode_count
+                if zone == 'anc': zone_count = anc_count
+                elif zone == 'census_tract': zone_count = census_count
+                elif zone == 'neighborhood_cluster': zone_count = cluster_count
+                elif zone == 'ward': zone_count = ward_count
+                elif zone == 'zip': zone_count = zipcode_count
 
                 if 'Warning' not in mar_return.keys():
                     flag = False
@@ -128,28 +143,31 @@ class CamaApiConn(BaseApiConn):
                     if not flag:
                         zone_count.append( OrderedDict([('zone_type', zone), ('zone', mar_return[zone]), ('housing_unit_count', num_units), ('bedroom_unit_count', bedrm)]) )
 
-        return {'ANC': anc_count, 'CENSUS': census_count, 'CLUSTER': cluster_count, 'WARD': ward_count, 'ZIPCODE': zipcode_count}
+        return {'anc': anc_count, 'census_tract': census_count, 'neighborhood_cluster': cluster_count, 'ward': ward_count, 'zip': zipcode_count}
+
 
     def get_csv(self):
+        """
+        Takes the returned dictionary from get_data() and convert the information
+        into csv file and then save the csv file in
+        housing-insights/data/processed/zoneUnitCount
+        as zoneUnitCount_2017-05-30.csv.
+
+        """
+
+        data_processed_zoneUnitCount = os.path.join(PYTHON_PATH, os.pardir, 'data', 'processed', 'zoneUnitCount')
+
         zone_data = self.get_data()
         toCSV = []
+        date = datetime.date.today().strftime('%Y-%m-%d')
+        filename = os.path.join(data_processed_zoneUnitCount, 'zoneUnitCount_'+date+'.csv')
 
         for key, value in zone_data.items():
-            # print("#"*100)
-            # print("#"*100)
-            # print("value: ", value)
             toCSV.extend(value)
-            # print("#"*100)
-            # print("#"*100)
-            # print("toCSV: ", toCSV)
 
         keys = toCSV[0].keys()
 
-        # print("#"*100)
-        # print("#"*100)
-        # print("keys: ", keys)
-
-        with open('camaData.csv', 'w') as output_file:
+        with open(filename, 'w') as output_file:
             dict_writer = csv.DictWriter(output_file, keys)
             dict_writer.writeheader()
             dict_writer.writerows(toCSV)
