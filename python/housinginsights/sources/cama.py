da11b57307032a6d882aff2375ef1cd4206537af
Naty Clementi <natyclementi@gmail.com>
2017-06-02 17:51:45 -0400

    Create zoneUnitCount dir if does not exists

--- python/housinginsights/sources/cama.py
+++ python/housinginsights/sources/cama.py
@@ -113,7 +113,7 @@ class CamaApiConn(BaseApiConn):
         """
 
         """ [:100] for the first 100 cama_data points """
-        for row in cama_data['features'][:2]:
+        for row in cama_data['features']:
             try:
                 objectid = row['properties']['OBJECTID']
                 if len(row['properties']['SSL']) == 8:
@@ -183,6 +183,9 @@ class CamaApiConn(BaseApiConn):
         as zoneUnitCount_2017-05-30.csv.
 
         """
+        if not os.path.exists('../../../data/processed/zoneUnitCount'):
+            os.makedirs('../../../data/processed/zoneUnitCount')
+
 
         data_processed_zoneUnitCount = os.path.join(PYTHON_PATH, os.pardir, 'data', 'processed', 'zoneUnitCount')
 
