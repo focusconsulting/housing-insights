65178bafea8e5f2941329de58bd8f309c4d7733e
Jason Haas <jasonrhaas@gmail.com>
2017-07-12 15:25:36 -0500

    SQL Automap and the base Flask RESTLESS api is working

--- python/api/declarative_app.py
+++ python/api/declarative_app.py
@@ -1,21 +1,60 @@
-from flask import Flask
-from flask_restless import APIManager
-from flask import jsonify
-from flask.json import JSONEncoder
 from datetime import datetime, date
+import json
 
-from models import MedianRent, Project, CensusMapping, Census
-from base import db, connect_str
+from flask import Flask, jsonify
+from flask.json import JSONEncoder
+from flask_restless import APIManager
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+from sqlalchemy import Boolean, Column, Date, DateTime, Integer, MetaData, Numeric, Table, Text
+
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+with open('secrets.json') as f:
+    secrets = json.load(f)
+    connect_str = secrets['docker_database']['connect_str']
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = connect_str
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Base = automap_base()
+
+metadata = MetaData(bind=db)
+
+Base.prepare(db.engine, reflect=True)
+
+db.session.commit()
+
+BuildingPermits = Base.classes.building_permits
+Census = Base.classes.census
+CensusMarginOfError = Base.classes.census_margin_of_error
+Crime = Base.classes.crime
+DcTax = Base.classes.dc_tax
+Project = Base.classes.project
+ReacScore = Base.classes.reac_score
+RealProperty = Base.classes.real_property
+Subsidy = Base.classes.subsidy
+Topa = Base.classes.topa
+WmataDist = Base.classes.wmata_dist
+WmataInfo = Base.classes.wmata_info
+
+models = [BuildingPermits, Census, CensusMarginOfError, Crime, DcTax, Project, ReacScore,
+          RealProperty, Subsidy, Topa, WmataDist, WmataInfo
+          ]
 
 db.init_app(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = connect_str
 
 manager = APIManager(app, flask_sqlalchemy_db=db)
 
-for model in [MedianRent, Project, CensusMapping, Census]:
+for model in models:
+    # https://github.com/jfinkels/flask-restless/pull/436
+    model.__tablename__ = model.__table__.name
     manager.create_api(model, methods=['GET'])
 
 
