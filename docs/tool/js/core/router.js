3d0391e5c790e31994300ce0291b2f36c221582b
John Osterman <ostermanj@gmail.com>
2017-08-19 14:39:11 -0400

    Encodes mapLayers other than the initial; decodes mapLayer before decoding filters

--- docs/tool/js/core/router.js
+++ docs/tool/js/core/router.js
@@ -6,7 +6,8 @@ var router = {
     initFilters: function(msg,data){
         if ( router.isFilterInitialized ) return;
         setSubs([
-            ['filterValues', router.pushFilter]
+            ['filterValues', router.pushFilter],
+            ['mapLayer', router.pushFilter]
         ]);        
         router.isFilterInitialized = true;
         if ( router.hasInitialFilterState ) router.decodeState();
@@ -24,7 +25,8 @@ var router = {
             router.stateObj[msg] = data;
             console.log(router.stateObj);
         }
-        if ( Object.keys(router.stateObj).length === 0 ) {
+        if ( Object.keys(router.stateObj).length === 0 || ( Object.keys(router.stateObj).length === 1 && router.stateObj.mapLayer === mapView.initialLayers[0].source ) ) {
+            // clear location has if state obj is empty  OR if it's only property is mapLayer equal to initial mapLayer (ward for now)
             window.history.replaceState(router.stateObj, 'newState', '#');
         } else {
             window.history.replaceState(router.stateObj, 'newState', '#/HI/' + router.paramifyFilter());        
@@ -40,27 +42,37 @@ var router = {
         var paramsArray = [];
         for (var key in router.stateObj) {
             if (!router.stateObj.hasOwnProperty(key)) continue;
-            var dataChoice = dataChoices.find(function(obj){
-                return key.split('.')[1] === obj.source;
-            });
-            
-            if ( dataChoice.component_type === 'continuous' ) {
-                var separator = router.stateObj[key] && router.stateObj[key][2] ? '-' : '_';
-                paramsArray.push(dataChoice.short_name + '=' + Math.round(router.stateObj[key][0]) + separator + Math.round(router.stateObj[key][1])); 
-            }
-            if ( dataChoice.component_type === 'categorical' || dataChoice.component_type === 'location'  ){
-                paramsArray.push( dataChoice.short_name + '=' + router.stateObj[key].join('+'));
-            }
-            if ( dataChoice.component_type === 'date' ){
-                var separator = router.stateObj[key] && router.stateObj[key][2] ? '-' : '_';
-                var dateStrings = [];
-                for ( var i = 0; i < 2; i++ ){ // i < 2 bc index 2 is true/false of nullsShown
-                    var date = router.stateObj[key][i].getDate();
-                    var month = router.stateObj[key][i].getMonth() + 1;
-                    var year = router.stateObj[key][i].getFullYear();
-                    dateStrings[i] = 'd' + date.toString() + 'm' + month.toString() + 'y' + year.toString(); 
+            if ( key.split('.')[0] === 'filterValues') {
+                var dataChoice = dataChoices.find(function(obj){
+                    return key.split('.')[1] === obj.source;
+                });
+                
+                if ( dataChoice.component_type === 'continuous' ) {
+                    var separator = router.stateObj[key] && router.stateObj[key][2] ? '-' : '_';
+                    paramsArray.push(dataChoice.short_name + '=' + Math.round(router.stateObj[key][0]) + separator + Math.round(router.stateObj[key][1])); 
                 }
-                paramsArray.push(dataChoice.short_name + '=' + dateStrings[0] + separator + dateStrings[1]);
+                if ( dataChoice.component_type === 'categorical' || dataChoice.component_type === 'location'  ){
+                    paramsArray.push( dataChoice.short_name + '=' + router.stateObj[key].join('+'));
+                }
+                if ( dataChoice.component_type === 'date' ){
+                    var separator = router.stateObj[key] && router.stateObj[key][2] ? '-' : '_';
+                    var dateStrings = [];
+                    for ( var i = 0; i < 2; i++ ){ // i < 2 bc index 2 is true/false of nullsShown
+                        var date = router.stateObj[key][i].getDate();
+                        var month = router.stateObj[key][i].getMonth() + 1;
+                        var year = router.stateObj[key][i].getFullYear();
+                        dateStrings[i] = 'd' + date.toString() + 'm' + month.toString() + 'y' + year.toString(); 
+                    }
+                    paramsArray.push(dataChoice.short_name + '=' + dateStrings[0] + separator + dateStrings[1]);
+                }
+            } else if ( key.split('.')[0] === 'mapLayer') {
+                if ( router.stateObj[key] !== mapView.initialLayers[0].source ) { // paramify only if mapLayer does not equal
+                                                                                  // initial layer. now `ward` but could be 
+                                                                                  // something else
+                    paramsArray.unshift('ml=' + router.stateObj[key]); // ensure mapLayer is always first
+                                                                       // some other component seems to already ensure
+                                                                       // this, but no harm done to do again.
+                }
             }
         }
      //   console.log(paramsArray.join('&'));
@@ -74,64 +86,60 @@ var router = {
         var stateArray = window.location.hash.replace('#/HI/','').split('&');
         stateArray.forEach(function(each){
             var eachArray = each.split('=');
-            var dataChoice = dataChoices.find(function(obj){
-                return obj.short_name === eachArray[0];
-            });
-            console.log('datachoice', dataChoice);
-            var filterControlObj = filterView.filterControlsDict[dataChoice.short_name]
-            console.log('filterControlObj', filterControlObj);
-            if ( dataChoice.component_type === 'continuous' ) {
-                var separator = eachArray[1].indexOf('-') !== -1 ? '-' : '_';
-                var values = eachArray[1].split(separator);
-                var min = +values[0]
-                var max = +values[1]
-                var nullsShown = separator === '_' ? false : true;
-                
-                filterControlObj.set(min,max,nullsShown);
-                
-            }
-            if ( dataChoice.component_type === 'categorical' || dataChoice.component_type === 'location' ){
-                console.log('categorical or location');
-                var values = eachArray[1].replace(/_/g,' ').split('+');
-                console.log(values);
-                if ( dataChoice.component_type === 'location' ) {
-                    /* below is very hard-coded to match the options of the location drodown with the association mapLayer */
-                    var expectedLayer = values[0].indexOf('Ward') !== -1 ? 'ward' : values[0].indexOf('Cluster') !== -1 ? 'neighborhood_cluster' : 'census_tract';
-                    if ( expectedLayer !== getState().mapLayer ) {
-                        setState('mapLayer',expectedLayer);
-                    }
+            if ( eachArray[0] === 'ml' ) { // ie if it's a mapLayer encoding
+                 setState('mapLayer',eachArray[1]);
+            } else {
+                var dataChoice = dataChoices.find(function(obj){
+                    return obj.short_name === eachArray[0];
+                });
+                console.log('datachoice', dataChoice);
+                var filterControlObj = filterView.filterControlsDict[dataChoice.short_name]
+                console.log('filterControlObj', filterControlObj);
+                if ( dataChoice.component_type === 'continuous' ) {
+                    var separator = eachArray[1].indexOf('-') !== -1 ? '-' : '_';
+                    var values = eachArray[1].split(separator);
+                    var min = +values[0]
+                    var max = +values[1]
+                    var nullsShown = separator === '_' ? false : true;
+                    
+                    filterControlObj.set(min,max,nullsShown);
+                    
                 }
-                setTimeout(function(){
-                    $('.ui.dropdown.'+'dropdown-' + dataChoice.source).dropdown('set selected', values);
-                }); // decoding location won't without the setTimeout trick, which asyncs the function, to be fired
-                    // in the next open slot in the queue. especially true if the mapLaye needs to be changed first, because then
-                    // probably a lot of async stuff is triggered but the setState call above. 
-            }   
-            if ( dataChoice.component_type === 'date' ){
-                // handle decoding for date type filter here
-                var separator = eachArray[1].indexOf('-') !== -1 ? '-' : '_';
-                var nullsShown = separator === '_' ? false : true;
-                var values = eachArray[1].split(separator);
+                if ( dataChoice.component_type === 'categorical' || dataChoice.component_type === 'location' ){
+                    console.log('categorical or location');
+                    var values = eachArray[1].replace(/_/g,' ').split('+');
+                    console.log(values);
+                    setTimeout(function(){
+                        $('.ui.dropdown.'+'dropdown-' + dataChoice.source).dropdown('set selected', values);
+                    }); // decoding location won't without the setTimeout trick, which asyncs the function, to be fired
+                        // in the next open slot in the queue. especially true if the mapLaye needs to be changed first, because then
+                        // probably a lot of async stuff is triggered but the setState call above. 
+                }   
+                if ( dataChoice.component_type === 'date' ){
+                    // handle decoding for date type filter here
+                    var separator = eachArray[1].indexOf('-') !== -1 ? '-' : '_';
+                    var nullsShown = separator === '_' ? false : true;
+                    var values = eachArray[1].split(separator);
 
-                //Set the values on the input boxes
-                filterView.filterInputs[dataChoice.short_name].setValues(
-                    [
-                        [ 'year',  values[0].match(/\d{4}/)[0] ],
-                        [ 'month', values[0].match(/m(\d+)/)[1] ],
-                        [ 'day',   values[0].match(/d(\d+)/)[1]]
-                    ],
-                    [
-                        [ 'year',  values[1].match(/\d{4}/)[0] ],
-                        [ 'month', values[1].match(/m(\d+)/)[1] ],
-                        [ 'day',   values[1].match(/d(\d+)/)[1]]
-                    ]
-                );
-                document.querySelector('[name="showNulls-' + dataChoice.source + '"]').checked = nullsShown;
-                
-                //Commit the values using callback(), which will update slider to match. 
-                filterView.filterInputs[dataChoice.short_name].callback();
+                    //Set the values on the input boxes
+                    filterView.filterInputs[dataChoice.short_name].setValues(
+                        [
+                            [ 'year',  values[0].match(/\d{4}/)[0] ],
+                            [ 'month', values[0].match(/m(\d+)/)[1] ],
+                            [ 'day',   values[0].match(/d(\d+)/)[1]]
+                        ],
+                        [
+                            [ 'year',  values[1].match(/\d{4}/)[0] ],
+                            [ 'month', values[1].match(/m(\d+)/)[1] ],
+                            [ 'day',   values[1].match(/d(\d+)/)[1]]
+                        ]
+                    );
+                    document.querySelector('[name="showNulls-' + dataChoice.source + '"]').checked = nullsShown;
+                    
+                    //Commit the values using callback(), which will update slider to match. 
+                    filterView.filterInputs[dataChoice.short_name].callback();
+                }
             }
-
         });
 
         this.clearScreen();
