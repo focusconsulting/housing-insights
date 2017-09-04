bb80563f23194f629cb88fb6c5a1c842d0e5fb25
John Kwening <kwening.john@gmail.com>
2017-09-04 16:27:07 -0400

    Additional address format parsing
    Updated 'get_unique_addresses_from_str()' to handle the following cases:
    - incomplete use of dash address
    - comma separated numbers
    - mixing of commas and & for numbers
    - mixing of commas and dash separated numbers with &
    - different meaning of &
    - cleanly handles what may be considered invalid address

--- python/housinginsights/tools/misc.py
+++ python/housinginsights/tools/misc.py
@@ -3,6 +3,7 @@
 from housinginsights.sources.mar import MarApiConn
 import logging
 
+
 def quick_address_cleanup(addr):
      #Used to perform common string replacements in addresses. 
         #The key is original, value is what we want it to become
@@ -19,7 +20,11 @@ def quick_address_cleanup(addr):
             " Pl ":" Place ",
             " Pl. ":" Place ",
             " Ave ":" Avenue ",
-            " Ave. ":" Avenue "
+            " Ave. ":" Avenue ",
+            "N.E.": "NE",
+            "N.W.": "NW",
+            "S.E.": "SE",
+            "S.W.": "SW"
         }
 
         # Format addr by matching the conventions of the MAR
@@ -27,6 +32,7 @@ def quick_address_cleanup(addr):
             addr = addr.replace(key,value)
 
         return addr
+
 
 def check_mar_for_address(addr, conn):
         '''
@@ -71,15 +77,19 @@ def check_mar_for_address(addr, conn):
         return None
 
 
+def get_unique_addresses_from_str(address_string=""):
+    """
+    Utility method for parsing an address string into a list of well
+    formatted unique addresses: "<address number> <street name> <quadrant>". The
+    process involves first parsing multiple address delimiters (semicolons and
+    " and ") into a list. Then for each result parsing address number range
+    delimiters to get a list of unique addresses.
 
-def get_unique_addresses_from_str(address_str=""):
+    :param address_string: the address string to be processed
+    :return: a list of unique addresses
+    """
 
-    def _trim_str(add_str):
-        """Helper function that does some simple string cleaning."""
-        add_str = add_str.lstrip()
-        add_str = add_str.rstrip()
-        return add_str
-
+    # Helper functions for multiple address delimiters parsing #
     def _parse_semicolon_delimiter(address_list):
         """Helper function that handles parsing semicolon delimiters"""
         output = list()
@@ -87,7 +97,7 @@ def get_unique_addresses_from_str(address_str=""):
             temp_results = add_str.split(sep=';')
             if len(temp_results) > 1:
                 for address in temp_results:
-                    output.append(_trim_str(address))
+                    output.append(address.strip())
             else:
                 output.append(add_str)
         return output
@@ -99,70 +109,146 @@ def get_unique_addresses_from_str(address_str=""):
             temp_results = add_str.split(sep=' and ')
             if len(temp_results) > 1:
                 for address in temp_results:
-                    output.append(_trim_str(address))
+                    output.append(address.strip())
             else:
                 output.append(add_str)
         return output
 
-    def _parse_ampersand_delimiter(address_list):
-        """Helper function that handles parsing '&' delimiters"""
-        output = list()
-        for add_str in address_list:
-            temp_results = add_str.split(sep='&')
-            if len(temp_results) > 1:
-                num_1 = _trim_str(temp_results[0])
-                base = _trim_str(temp_results[1])
-                num_2, base = base.split(' ', 1)
-                num_2 = _trim_str(num_2)
-                base = _trim_str(base)
+    # Begin parsing process #
+    # used to track resulting addresses from multiple address delimiter parsing
+    multiple_addresses_result = [address_string]
 
-                output.append('{} {}'.format(num_1, base))
-                output.append('{} {}'.format(num_2, base))
+    # 1: first parse multiple address delimiters: ';', 'and'
+    multiple_addresses_result = _parse_semicolon_delimiter(
+        multiple_addresses_result)
+    multiple_addresses_result = _parse_and_delimiter(
+        multiple_addresses_result)
+
+    # 2: second parse address numbers delimiters #
+    unique_addresses_result = list()
+
+    # iterate through results from parsing multiple address delimiters
+    for addr_str in multiple_addresses_result:
+        # handle 'other's special case
+        if addr_str == 'others':
+            unique_addresses_result.append(addr_str)
+            continue
+
+        delimiter_stack = list()  # used to track delimiters within addr_str
+        str_stack = list()  # used to track string literals in addr_str
+
+        # populate delimiter_stack and str_stack accordingly from addr_str
+        _str = ''
+        for char in addr_str:
+            if char in {'-', ',', '&'}:
+                if len(_str) > 0:  # add current str partition into stack
+                    str_stack.append(_str)
+                    _str = ''  # restart str tracking
+                delimiter_stack.append(char)
             else:
-                output.append(add_str)
-        return output
+                _str += char
 
-    def _parse_dash_delimiter(address_list):
-        """Helper function that handles parsing '-' delimiters"""
-        output = list()
-        for add_str in address_list:
-            temp_results = add_str.split(sep='-')
-            if len(temp_results) > 1:
-                num_1 = _trim_str(temp_results[0])
-                base = _trim_str(temp_results[1])
-                num_2, base = base.split(' ', 1)
-                num_2 = _trim_str(num_2)
-                base = _trim_str(base)
+        # validate stacks to handle contiguous range delimiter cases
+        if len(delimiter_stack) > len(str_stack):
+            logging.warning('Invalid address: {}'.format(
+                addr_str))
+            unique_addresses_result.append(addr_str)
+            continue
 
-                #TODO is this properly handling a bad address range situation??
-                if not isinstance(num_1,int) or not isinstance(num_2,int):
-                    #there was some parseing problem - throw it back
-                    output.append(add_str)
+        base_str = _str.strip()  # initialize base to be remaining _str val
+
+        # Determine 'address_num' and '<streetname> <quadrant' base for
+        # generating all unique addresses for addr_str. If it cannot be
+        # partitioned accordingly return addr_str as-is
+        try:
+            addr_num, base = base_str.split(' ', 1)
+            _ = int(addr_num)  # validate address number partition
+            unique_addresses_result.append(base_str)
+        except ValueError:
+            # handle '<streetname>, <quadrant>' address cases
+            delim = delimiter_stack.pop()
+
+            if delim == ',':
+                _str = str_stack.pop().strip()
+
+                # flag '<address_num>, <streetname> <quadrant>' as invalid case
+                try:
+                    _ = int(_str)
+                    logging.warning('Invalid address: {}'.format(
+                        addr_str))
+                    unique_addresses_result.append(addr_str)
                     continue
+                except ValueError:
+                    base_str = '{} {}'.format(_str, base_str)
+                    addr_num, base = base_str.split(' ', 1)
+                    unique_addresses_result.append(base_str)
+
+            else:  # fail cleanly for all other invalid cases
+                logging.warning('Invalid address: {}'.format(
+                    addr_str))
+                unique_addresses_result.append(addr_str)
+                continue
+
+        num = addr_num  # reset prior to parsing new 'addr_str' batch
+
+        while delimiter_stack:
+            # get next delimiter
+            delim = delimiter_stack.pop()
+
+            if delim == ',':
+                num = str_stack.pop().strip()
+                unique_addresses_result.append('{} {}'.format(num, base))
+
+            elif delim == '-':
+                shift_range_by = 0  # exclude original base address number?
+                # handle composites - check num exists and assign accordingly
+                try:
+                    addr_num = num
+                    num = str_stack.pop().strip()
+                except NameError as e:
+                    logging.warning(
+                        'Missing "num" - using default addr_num: {}'.format(e))
+                    num = str_stack.pop().strip()
+
+                # check range style and convert accordingly 1210-12 vs 1210-1212
+                if len(num) > len(addr_num):
+                    diff = len(num) - len(addr_num)
+                    addr_num = num[:diff] + addr_num
+                    # remove added based_str in final_pass_result and shift range by 1
+                    _ = unique_addresses_result.pop()
+                    shift_range_by += 1
 
                 # check whether odd, even, or ambiguous range
-                even = True if int(num_1) % 2 == 0 else False
+                even = True if int(num) % 2 == 0 else False
 
-                if (int(num_2) % 2 == 0 and not even) or (
-                            int(num_2) % 2 != 0 and even):
+                if (int(addr_num) % 2 == 0 and not even) or (
+                                    int(addr_num) % 2 != 0 and even):
                     even = None
 
                 # populate address number ranges
                 step = 1 if even is None else 2
-                for num in range(int(num_1), int(num_2) + 1, step):
-                    output.append('{} {}'.format(num, base))
-            else:
-                output.append(add_str)
-        return output
 
-    result = [address_str]  # tracks unique addresses from address_str
+                for cur_num in range(int(num), int(addr_num) + shift_range_by,
+                                     step):
+                    unique_addresses_result.append('{} {}'.format(cur_num, base))
 
-    # 1: parse complete address delimiters - ';', 'and'
-    result = _parse_semicolon_delimiter(result)
-    result = _parse_and_delimiter(result)
+            elif delim == '&':
+                num = str_stack.pop().strip()
 
-    # 2: parse address number range delimiters  - '&', '-'
-    result = _parse_ampersand_delimiter(result)
-    result = _parse_dash_delimiter(result)
+                if num == '':  # skip if next in str is empty or space
+                    continue
 
-    return result
+                try:
+                    # check if actually number, else return original
+                    _ = int(num)
+                    unique_addresses_result.append('{} {}'.format(num, base))
+                except ValueError:
+                    # remove previous result that was added incorrectly
+                    _ = unique_addresses_result.pop()
+                    logging.warning('Invalid address: {} & {}'.format(
+                        num, base_str))
+                    unique_addresses_result.append('{} & {}'.format(
+                        num, base_str))
+
+    return unique_addresses_result
+
