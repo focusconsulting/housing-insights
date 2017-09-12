855af15df6088a91d0c949d5f133299755e9bd15
Kunal Marwaha <marwahaha@berkeley.edu>
2017-09-11 23:21:34 -0400

    Cleanup

--- docs/js/datasource.js
+++ docs/js/datasource.js
@@ -5,10 +5,8 @@ $(function () {
         dataType: "json",
         url: "http://housinginsights.us-east-1.elasticbeanstalk.com/api/meta",
         success: function (data) {
-            console.log(data);
             // This is the ID (accessed from a hidden div in the template)
             var id = document.getElementById(containerDivId).childNodes[0].id;
-            console.log(id);
             if (!data[id]) {
                 console.log("There is no data for this datasource! Please add some.");
                 return false;
@@ -16,9 +14,9 @@ $(function () {
 
             // from https://stackoverflow.com/questions/15164655/generate-html-table-from-2d-javascript-array
             var table = document.createElement('table');
-            var tableBody = document.createElement('tbody');
 
             // Create the headers
+            var tableHead = document.createElement('thead');
             var headerRow = document.createElement('tr');
 
             var headerDisplayName = document.createElement('th');
@@ -28,8 +26,11 @@ $(function () {
             var headerDisplayText = document.createElement('th');
             headerDisplayText.appendChild(document.createTextNode("Display Text"));
             headerRow.appendChild(headerDisplayText);
-            tableBody.appendChild(headerRow);
 
+            tableHead.appendChild(headerRow);
+            table.appendChild(tableHead);
+
+            var tableBody = document.createElement('tbody');
             data[id].fields.forEach(function(rowData) {
               var row = document.createElement('tr');
 
