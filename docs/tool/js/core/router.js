5755436578474e5c5ab25092c741b375b967a2bf
John Osterman <ostermanj@gmail.com>
2017-09-15 09:54:47 -0400

    Remove debugging logs

--- docs/tool/js/core/router.js
+++ docs/tool/js/core/router.js
@@ -151,15 +151,10 @@ var router = {
                 setState('previewBuilding', [matchingRenderedProject,true]); // true = flag to scroll matching projects list
             
             } else {
-                console.log('DEBUGGING', eachArray);
                 dataChoice = dataChoices.find(function(obj){
                     return obj.short_name === eachArray[0];
                 });
-                console.log('datachoice', dataChoice);
-                console.log('filterControlsDict',filterView.filterControlsDict);
-                console.log('filterViewLoaded',getState().filterViewLoaded);
                 var filterControlObj = filterView.filterControlsDict[dataChoice.short_name]
-                console.log('filterControlObj', filterControlObj);
                 if ( dataChoice.component_type === 'continuous' ) {
                     var separator = eachArray[1].indexOf('-') !== -1 ? '-' : '_';
                     var values = eachArray[1].split(separator);
