98904dcf1a6500418a08c0e1d3b94a6b1ca7ddd6
Nick Tallant <ndtallant@uchicago.edu>
2019-08-29 19:47:33 -0500

    All fields accounted for census

--- ETL/census.py
+++ ETL/census.py
@@ -16,6 +16,15 @@ base = f'https://api.census.gov/data/{year}/acs/acs5'
 
 fields = {
     'B01003_001E': 'total_population',
+    'B02001_003E': 'black_population',
+    'B17020_002E': 'population_in_poverty',
+    'B23025_002E': 'labor_force_population',
+    'B16008_019E': 'foreign_born_population',
+    'B09002_015E': 'single_mom_households',
+    'B19025_001E': 'aggregate_household_income',
+    'B25057_001E': 'lower_rent_quartile_in_dollars',
+    'B25058_001E': 'median_rent_quartile_in_dollars',
+    'B25059_001E': 'upper_rent_quartile_in_dollars',
 }
 
 response = requests.get(base,
@@ -26,8 +35,11 @@ response = requests.get(base,
             'key': key,
             }
 )
-print(response)
-data = response.json()
-df = pd.DataFrame(data[1:], columns=data[0])
-df = df.rename(columns=fields)
-print(df.head())
+
+if response.status_code == 200:
+    data = response.json()
+    df = pd.DataFrame(data[1:], columns=data[0])
+    df = df.rename(columns=fields)
+    print(df.head())
+else:
+    print("Could not query")
