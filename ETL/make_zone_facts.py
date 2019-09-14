f0d0c4ac570ee83ef8cb5af662b6998e4707ecd7
Nick Tallant <ndtallant@uchicago.edu>
2019-09-14 16:38:10 -0500

    Need to do ward and cluster for permit and acs

--- ETL/make_zone_facts.py
+++ ETL/make_zone_facts.py
@@ -27,12 +27,14 @@ from sources import get_acs_data, get_crime_data, get_permit_data
 
 print("Grabbing ACS data")
 acs = get_acs_data()
+#acs_tract, acs_cluster, acs_ward = get_acs_data()
 
 print("Grabbing permit data")
 permit = get_permit_data()
+#permit_tract, permit_cluster, permit_ward = get_permit_data()
 
 print("Grabbing crime data")
-crime = get_crime_data()
+crime_tract, crime_cluster, crime_ward = get_crime_data()
 
 df = acs.merge(permit, left_on='tract', right_on='tract')
 df = df.merge(crime, left_on='tract', right_on='census_tract')
