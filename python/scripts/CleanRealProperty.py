9f2b375d2261c530ddfecdb7bd01778c08cfd995
Michael Kalish <michael.kalish@focusconsulting.io>
2024-03-19 21:37:53 -0400

    Preservation catalog, tax, crime, building permits and census data updates

--- python/scripts/CleanRealProperty.py
+++ python/scripts/CleanRealProperty.py
@@ -1,4 +1,3 @@
-
 # Camel case all the headers
 # Make the fields line up
 #
@@ -9,18 +8,21 @@ import numpy as np
 
 
 def createProperHeader(header):
-    return header.replace('_', ' ').title().replace(' ', '_')
+    return header.replace("_", " ").title().replace(" ", "_")
 
 
-results_dir = os.path.join(os.path.dirname(
-    __file__), "../../data/raw/preservation_catalog/20220620")
+results_dir = os.path.join(
+    os.path.dirname(__file__), "../../data/raw/preservation_catalog/20240301"
+)
 df = pd.read_csv(
-    '../../data/raw/preservation_catalog/20220620/Real_property.csv', encoding='windows-1252')
+    "../../data/raw/preservation_catalog/20240301/Real_property.csv",
+    encoding="windows-1252",
+)
 updated_col_names = {}
 df.replace(to_replace="U", value="01/01/1970", inplace=True)
 df.replace(to_replace="N", value="01/01/1970", inplace=True)
 
 
-df.to_csv(results_dir + '/Real_property.csv', encoding="utf-8", index=False)
+df.to_csv(results_dir + "/Real_property.csv", encoding="utf-8", index=False)
 
 print("Done")
